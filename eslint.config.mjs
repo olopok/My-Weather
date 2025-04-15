fc75f4276575ef44b62c5ee8c895bb56bb6c308d
olopok <pabisa.pb@gmail.com>
2025-04-15 16:38:47 +0100

    Starting set up the repository.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  eslintConfigPrettier,
 ]);
